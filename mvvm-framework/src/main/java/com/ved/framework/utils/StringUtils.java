a517329fed2eeb514c7d57c41e1a87c870f977b6
lixiong <qqlixiongjob@163.com>
2023-10-12 13:12:28 +0800

    ......

--- mvvm-framework/src/main/java/com/ved/framework/utils/StringUtils.java
+++ mvvm-framework/src/main/java/com/ved/framework/utils/StringUtils.java
@@ -423,4 +423,35 @@ public final class StringUtils {
         }
     }
 
+    public String calculateTime(int time) {
+        int minute;
+        int second;
+        if (time >= 60) {
+            minute = time / 60;
+            second = time % 60;
+            //分钟在0~9
+            if (minute < 10) {
+                //判断秒
+                if (second < 10) {
+                    return "0" + minute + ":" + "0" + second;
+                } else {
+                    return "0" + minute + ":" + second;
+                }
+            } else {
+                //分钟大于10再判断秒
+                if (second < 10) {
+                    return minute + ":" + "0" + second;
+                } else {
+                    return minute + ":" + second;
+                }
+            }
+        } else {
+            second = time;
+            if (second >= 0 && second < 10) {
+                return "00:" + "0" + second;
+            } else {
+                return "00:" + second;
+            }
+        }
+    }
 }
