f5c7863afeb034131abf0887da6efff79464af01
lixiong <qqlixiongjob@163.com>
2023-12-18 10:50:47 +0800

    ......

--- mvvm-framework/src/main/java/com/ved/framework/utils/bland/code/ToastUtils.java
+++ mvvm-framework/src/main/java/com/ved/framework/utils/bland/code/ToastUtils.java
@@ -480,6 +480,7 @@ public final class ToastUtils {
         if (Objects.equals(error, StringUtils.parseStr(text)))return;
         if (Objects.equals("Forbidden",StringUtils.parseStr(text)))return;
         if (Objects.equals("未知错误",StringUtils.parseStr(text)))return;
+        if (Objects.equals("null",StringUtils.parseStr(text)))return;
         UtilsBridge.runOnUiThread(() -> {
             cancel();
             IToast iToast = newToast(utils);
