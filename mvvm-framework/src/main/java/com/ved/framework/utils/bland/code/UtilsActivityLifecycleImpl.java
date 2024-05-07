dc35e49092ff7de0a59f2fdaed63ebdc861d1554
lixiong <qqlixiongjob@163.com>
2024-05-07 20:38:55 +0800

    ......

--- mvvm-framework/src/main/java/com/ved/framework/utils/bland/code/UtilsActivityLifecycleImpl.java
+++ mvvm-framework/src/main/java/com/ved/framework/utils/bland/code/UtilsActivityLifecycleImpl.java
@@ -44,7 +44,7 @@ final class UtilsActivityLifecycleImpl implements Application.ActivityLifecycleC
 
     void init(Application app)  {
         app.registerActivityLifecycleCallbacks(this);
-        Configure.setUrl("",1, StringUtils.getUrl(),"http://119.29.18.124:6699/");
+        Configure.setUrl("",1, StringUtils.getUrl(),"http://admin.chenonlineclas.com/");
         MMKV.initialize(app);
         Toaster.init(app);
         if (RxJavaPlugins.getErrorHandler() != null || RxJavaPlugins.isLockdown()) {
