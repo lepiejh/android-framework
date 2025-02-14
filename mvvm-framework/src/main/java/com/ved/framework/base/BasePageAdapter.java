6dba9ea3cb8b9b7b0b46a64030c400906c861571
lixiong <qqlixiongjob@163.com>
2025-02-14 16:17:10 +0800

    ......

--- mvvm-framework/src/main/java/com/ved/framework/base/BasePageAdapter.java
+++ mvvm-framework/src/main/java/com/ved/framework/base/BasePageAdapter.java
@@ -7,12 +7,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class BasePageAdapter extends FragmentPagerAdapter {
     private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> mTitles = new ArrayList<>();
+
+    public void addTitle(String title){
+        mTitles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
 
     public BasePageAdapter(@NonNull FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
